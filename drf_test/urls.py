c49235f90ca0308a52da8ee84407e3aaabe70af3
Action2theFuture <dnstks0204@gmail.com>
2021-10-11 16:54:51 +0900

    Add Prometheus url

--- drf_test/urls.py
+++ drf_test/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url('', include('django_prometheus.urls')),
+    path('', include('django_prometheus.urls')),
     path('user/', include('user.urls'))
 ]
