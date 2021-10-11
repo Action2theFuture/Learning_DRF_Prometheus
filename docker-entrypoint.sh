55473e9093e1048c202c5a29b60a5a4cb30485b1
Action2theFuture <dnstks0204@gmail.com>
2021-10-11 15:00:21 +0900

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,9 @@ python manage.py collectstatic --noinput
 echo "Apply database migrations"
 python manage.py migrate
 
+echo "Add datafolder"
+mkdir data
+
 # Start server
 echo "Starting server"
 gunicorn --bind 0.0.0.0:8000 drf_test.wsgi:application
